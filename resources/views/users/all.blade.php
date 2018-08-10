83470a4671b8306016d72601fdb5321096e6a77c
chtotonetak <madlabman@mail.ru>
2018-08-10 10:24:45 +0400

    Fix token count change [TB-7]

--- resources/views/users/all.blade.php
+++ resources/views/users/all.blade.php
@@ -1,52 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="uk-container uk-padding">
-        <h2 class="uk-text-center">
-            Пользователи
-        </h2>
-        <div class="uk-text-center">
-            <a href="{{ url('/users/new') }}" class="uk-button uk-button-primary">Добавить пользователя</a>
-        </div>
+<div class="uk-container uk-padding">
+    <h2 class="uk-text-center">
+        Пользователи
+    </h2>
+    <div class="uk-text-center">
+        <a href="{{ url('/users/new') }}" class="uk-button uk-button-primary">Добавить пользователя</a>
     </div>
+</div>
 
-    <div class="uk-container uk-padding">
+<div class="uk-container uk-padding">
 
-                <table class="uk-table uk-table-middle">
-                    <thead>
-                        <tr>
-                            <th>Пользователь</th>
-                            <th>Тело</th>
-                            <th>Бонус</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($users as $user)
-                        <tr>
-                            <form action="{{ url()->to('/users/quick-update') }}" method="post">
-                                <td>{{ $user->name }} [<em><small>{{ $user->phone }}</small></em>]</td>
-                                {{ csrf_field() }}
-                                <input type="hidden" value="{{ $user->id }}" name="user">
-                                <td><input type="number" value="{{ $user->balance->body }}" name="body" class="uk-input"></td>
-                                <td><input type="number" value="{{ $user->balance->bonus }}" name="bonus" class="uk-input"></td>
-                                <td>
-                                    <button type="submit" class="uk-button uk-button-primary uk-button-small">Обновить</button>
-                                    <a href="{{ url('/users/edit/' . $user->id) }}" class="uk-button uk-button-primary uk-button-small">Редактировать</a>
-                                    <a href="{{ url('/users/delete/' . $user->id) }}" class="uk-button uk-button-danger uk-button-small">Удалить</a>
-                                </td>
-                            </form>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+    <table class="uk-table uk-table-middle">
+        <thead>
+        <tr>
+            <th>Пользователь</th>
+            <th>Тело</th>
+            <th>Бонус</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+        <tr>
+            <form action="{{ url()->to('/users/quick-update') }}" method="post">
+                <td>{{ $user->name }} [<em>
+                        <small>{{ $user->phone }}</small>
+                    </em>]
+                </td>
+                {{ csrf_field() }}
+                <input type="hidden" value="{{ $user->id }}" name="user">
+                <td>
+                    <input type="number" value="{{ $user->balance->body }}" name="body" class="uk-input" step="any">
+                </td>
+                <td>
+                    <input type="number" value="{{ $user->balance->bonus }}" name="bonus" class="uk-input" step="any">
+                </td>
+                <td>
+                    <button type="submit" class="uk-button uk-button-primary uk-button-small">Обновить</button>
+                    <a href="{{ url('/users/edit/' . $user->id) }}" class="uk-button uk-button-primary uk-button-small">Редактировать</a>
+                    <a href="{{ url('/users/delete/' . $user->id) }}"
+                       class="uk-button uk-button-danger uk-button-small">Удалить</a>
+                </td>
+            </form>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
 
-        @if(!empty($pages))
-            <ul class="uk-pagination uk-flex-center uk-padding">
-                @foreach($pages as $page)
-                    <li class="@if($page['active']){{ 'uk-active' }}@endif"><a href="{{ $page['link'] }}">{{ $page['text'] }}</a></li>
-                @endforeach
-            </ul>
-        @endif
-    </div>
+    @if(!empty($pages))
+    <ul class="uk-pagination uk-flex-center uk-padding">
+        @foreach($pages as $page)
+        <li class="@if($page['active']){{ 'uk-active' }}@endif"><a href="{{ $page['link'] }}">{{ $page['text'] }}</a>
+        </li>
+        @endforeach
+    </ul>
+    @endif
+</div>
 @endsection
